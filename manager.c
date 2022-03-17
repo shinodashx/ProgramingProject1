693cb655d3770d54a1ca925ac8f3609535ad2f7c
shinodashx <1323837835@qq.com>
2022-03-17 20:24:29 +0800

    Update the two programs, user and manager, to roughly construct the functional framework.

--- manager.c
+++ manager.c
@@ -1,4 +1,103 @@
-//
-// Created by shinoda_shx on 15/3/2022.
-//
+#include <stdio.h>
+#include <stdlib.h>
+#include "book_management.h"
 
+
+int add_book(Book book){
+
+}
+
+int remove_book(Book book){
+
+}
+
+
+BookList find_book_by_title (const char *title){
+
+}
+
+BookList find_book_by_author (const char *author){
+
+}
+
+
+BookList find_book_by_year (unsigned int year){
+
+
+}
+
+
+void userinterfscea(){
+    while(1) {
+        printf("Please choose your choice:(Input choice number)");
+        printf("1.Add book");
+        printf("2.Remove book");
+        printf("3.Find book by title.");
+        printf("4.Find book by author");
+        printf("5.Find book by year");
+        printf("6.Borrow book");
+        printf("7.Return book");
+        printf("8.List all books in library");
+        printf("9.exit");
+        int maOp;
+        scanf("%d",&maOp);
+        int usFlag = 1;
+        while(1){
+            if(maOp>=1&&maOp<=9) break;
+            else {
+                printf("Error choice number");
+                scanf("%d", &maOp);
+            }
+        }
+        if(maOp == 1){
+            printf("");
+            char bookTitle[100];
+            char bookAuthor[100];
+            int bookYear;
+            scanf("%s", bookTitle);
+            scanf("%s",bookAuthor);
+            scanf("%d",bookYear);
+
+        }
+        if(maOp == 2{
+            printf("");
+            char bookTitle[100];
+            char bookAuthor[100];
+            int bookYear;
+            scanf("%s", bookTitle);
+            scanf("%s",bookAuthor);
+            scanf("%d",bookYear);
+        }
+        if(maOp == 3){
+            printf("Please input book title");
+            char bookTitle[100];
+            scanf("%s", bookTitle);
+            find_book_by_title(bookTitle);
+        }
+        if(maOp == 4){
+            printf("Please input book author");
+            char author[100];
+            scanf("%s", author);
+            find_book_by_author(author);
+        }
+        if(maOp == 5){
+            printf("Please input book year");
+            int year;
+            scanf("%d", year);
+            find_book_by_year(year);
+        }
+        if(maOp == 4){
+
+
+        }
+        if(maOp == 5){
+
+        }
+        if(maOp == 6){
+
+        }
+        if(maOp == 7) exit(0);
+
+    }
+
+}
--- user.c
+++ user.c
@@ -1,6 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "book_management.h"
 
-void log(){
+
+BookList find_book_by_title (const char *title){
+
+}
+
+BookList find_book_by_author (const char *author){
+
+}
+
+
+BookList find_book_by_year (unsigned int year){
+
+
+}
+
+
+void userinterfscea(){
+    while(1) {
+        printf("Please choose your choice:(Input choice number)");
+        printf("1.Find book by title.");
+        printf("2.Find book by author");
+        printf("3.Find book by year");
+        printf("4.Borrow book");
+        printf("5.Return book");
+        printf("6.List all books in library");
+        printf("7.exit");
+        int usOp;
+        scanf("%d",&usOp);
+        int usFlag = 1;
+        while(1){
+            if(usOp>=1&&usOp<=6) break;
+            else {
+                printf("Error choice number");
+                scanf("%d", &usOp);
+            }
+        }
+        if(usOp == 1){
+            printf("Please input book title");
+            char bookTitle[100];
+            scanf("%s", bookTitle);
+            find_book_by_title(bookTitle);
+        }
+        if(usOp == 2){
+            printf("Please input book author");
+            char author[100];
+            scanf("%s", author);
+            find_book_by_author(author);
+        }
+        if(usOp == 3){
+            printf("Please input book year");
+            int year;
+            scanf("%d", year);
+            find_book_by_year(year);
+        }
+        if(usOp == 4){
+
+
+        }
+        if(usOp == 5){
+
+        }
+        if(usOp == 6){
+
+        }
+        if(usOp == 7) exit(0);
+
+    }
 
 }
